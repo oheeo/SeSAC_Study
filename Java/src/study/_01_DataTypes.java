017560a1bf9bac8e8663cdc78bd1aefa897899b1
dahee oh <ekgml4122@gmail.com>
2023-06-11 18:26:54 +0900

    class study commit

---
+++ Java/src/study/_01_DataTypes.java
@@ -0,0 +1,17 @@
+package study;
+
+public class _01_DataTypes {
+
+    public static void main(String[] args) {
+        System.out.println("안녕");
+        System.out.println(4 + "랑해");
+
+        int a = 4;
+        String b = "랑해";
+        System.out.println(a+b);
+
+        System.out.printf("%d\n",30);  // 10진수
+        System.out.printf("%o\n",30);  // 8진수
+        System.out.printf("%x\n",30);  // 16진수
+    }
+}

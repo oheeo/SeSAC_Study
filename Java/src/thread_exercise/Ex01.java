b2b90937d826471e64ba8f0e9881d86c794cebac
dahee oh <ekgml4122@gmail.com>
2023-06-19 22:02:54 +0900

    쓰레드 객체 생성 후 실행

---
+++ Java/src/thread_exercise/Ex01.java
@@ -0,0 +1,21 @@
+package thread_exercise;
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(i + "초");
+        }
+    }
+}
+
+class Ex01 {
+    public static void main(String[] args) {
+            MyThread1 mt1 = new MyThread1();
+            mt1.start();
+        }
+    }

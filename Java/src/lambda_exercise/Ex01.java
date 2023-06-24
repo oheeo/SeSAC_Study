18824ddc04bb9c818a3a2b9c9868430443bec68d
dahee oh <ekgml4122@gmail.com>
2023-06-24 22:55:00 +0900

    Java Lambda Exercise

---
+++ Java/src/lambda_exercise/Ex01.java
@@ -0,0 +1,21 @@
+package lambda_exercise;
+
+interface A1 {
+    void abc(double k);
+}
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+//        A a = new A() {
+//            public void abc(double k) {
+//                System.out.println(k + 0.5);
+//            };
+//        };
+//        a.abc(3.8);
+
+        A1 a = (double k)->{System.out.println(k + 0.5);};
+        a.abc(3.8);
+        // 익명이너클래스 표현과 람다식의 경우 중괄호() 다음에 세미콜론이 옴
+    }
+}

04c7fc79517631c721faec63b84d5c189f3814f7
dahee oh <ekgml4122@gmail.com>
2023-06-30 22:42:43 +0900

    Java TCP-Client와 Server간의 Text 교환

---
+++ Java/src/TCP/_01_.java
@@ -0,0 +1,32 @@
+package TCP;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+
+public class _01_ {
+    public static void main(String[] args) {
+        System.out.println("<<Client>>");
+        try {
+            Socket socket = new Socket(InetAddress.getByName("localhost"),
+                    10000);
+            System.out.println("Server에 접속 완료");
+            System.out.println("접속 Server 주소 : "+socket.getInetAddress()+":"+socket.getPort());
+            DataInputStream dis = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            DataOutputStream dos = new DataOutputStream(
+                    new BufferedOutputStream(socket.getOutputStream()));
+            dos.writeUTF("안녕하세요");
+            dos.flush();
+            String str = dis.readUTF();
+            System.out.println("server : "+str);
+        }
+        catch (UnknownHostException e) {}
+        catch (IOException e) {e.printStackTrace();}
+    }
+}

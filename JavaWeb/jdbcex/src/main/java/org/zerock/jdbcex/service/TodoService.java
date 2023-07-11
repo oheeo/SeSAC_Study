68285798f5dfdb70dbdeae381eb06c9b405e9524
dahee oh <ekgml4122@gmail.com>
2023-07-11 21:56:18 +0900

    자바 웹 개발 2장-수정 기능 구현

--- JavaWeb/jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ JavaWeb/jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -49,4 +49,15 @@ public enum TodoService {
         return todoDTO;
     }
 
+    public void remove(Long tno)throws Exception {
+        log.info("tno: " + tno);
+        dao.deleteOne(tno);
+    }
+
+    public void modify(TodoDTO todoDTO) throws Exception {
+        log.info("todoDTO: " + todoDTO );
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+        dao.updateOne(todoVO);
+    }
+
 }

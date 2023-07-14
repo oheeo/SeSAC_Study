5c4be1ff5a3c9daeb5bd4b934b7c0aba83566acb
dahee oh <ekgml4122@gmail.com>
2023-07-14 20:20:15 +0900

    자바 웹 개발 3장-세션/쿠키/필터/리스너

--- JavaWeb/w2/src/main/java/org/zerock/w2/controller/LoginController.java
+++ JavaWeb/w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -1,14 +1,14 @@
 package org.zerock.w2.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.zerock.w2.dto.MemberDTO;
+import org.zerock.w2.service.MemberService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/login")
 @Log4j2
@@ -22,6 +22,24 @@ public class LoginController extends HttpServlet {
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
     }
 
+    //    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//        log.info("login post........");
+//
+//        String mid = req.getParameter("mid");
+//        String mpw = req.getParameter("mpw");
+//
+//        String str = mid+mpw;
+//
+//        HttpSession session = req.getSession();
+//
+//        session.setAttribute("loginInfo", str);
+//
+//        resp.sendRedirect("/todo/list");
+//
+//    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -30,14 +48,38 @@ public class LoginController extends HttpServlet {
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
 
-        String str = mid+mpw;
+        String auto  = req.getParameter("auto");
 
-        HttpSession session = req.getSession();
+        boolean rememberMe = auto != null && auto.equals("on");
 
-        session.setAttribute("loginInfo", str);
+        try {
+            MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
 
-        resp.sendRedirect("/todo/list");
+            if(rememberMe){
+                String uuid = UUID.randomUUID().toString();
 
+                MemberService.INSTANCE.updateUuid(mid, uuid);
+                memberDTO.setUuid(uuid);
+
+                Cookie rememberCookie =
+                        new Cookie("remember-me", uuid);
+                rememberCookie.setMaxAge(60*60*24*7);  //  쿠키의 유효기간은 1주일
+                rememberCookie.setPath("/");
+
+                resp.addCookie(rememberCookie);
+
+            }
+
+
+            HttpSession session = req.getSession();
+
+            session.setAttribute("loginInfo", memberDTO);
+
+            resp.sendRedirect("/todo/list");
+
+        } catch (Exception e) {
+            resp.sendRedirect("/login?result=error");
+        }
     }
 
 
--- JavaWeb/w2/src/main/webapp/WEB-INF/todo/list.jsp
+++ JavaWeb/w2/src/main/webapp/WEB-INF/todo/list.jsp
@@ -14,6 +14,11 @@
 </head>
 <body>
 <h1>Todo List</h1>
+
+<h2>${appName}</h2>
+<h2>${loginInfo}</h2>
+<h3>${loginInfo.mname}</h3>
+
 <ul>
     <c:forEach items="${dtoList}" var="dto">
         <li>
@@ -24,5 +29,9 @@
         </li>
     </c:forEach>
 </ul>
+
+<form action="/logout" method="post">
+    <button>LOGOUT</button>
+</form>
 </body>
 </html>

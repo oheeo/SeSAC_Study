9b2ae7274a94dd99a1e442a2244777dc6bfae899
dahee oh <ekgml4122@gmail.com>
2023-07-22 21:19:39 +0900

    자바 웹 개발 워크북 4.4 스프링 Web MVC 구현

--- JavaWeb/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ JavaWeb/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -67,4 +67,13 @@ public class TodoController {
         model.addAttribute("dto", todoDTO);
     }
 
+    @PostMapping("/remove")
+    public String remove(Long tno, RedirectAttributes redirectAttributes) {
+
+        log.info("---------------remove----------------");
+        log.info("tno: " + tno);
+
+        return "redirect:/todo/list";
+    }
+
 }

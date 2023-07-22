0ae14691ded2f834021c41123590fc0c520a2af5
dahee oh <ekgml4122@gmail.com>
2023-07-22 22:57:32 +0900

    자바 웹 개발 워크북 4.4 스프링 Web MVC 구현-페이징

---
+++ JavaWeb/springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
@@ -0,0 +1,22 @@
+package org.zerock.springex.dto;
+
+import java.util.List;
+public class PageResponseDTO<E> {
+
+    private int page;
+    private int size;
+    private int total;
+
+    //시작 페이지 번호
+    private int start;
+    //끝 페이지 번호
+    private int end;
+
+    //이전 페이지의 존재 여부
+    private boolean prev;
+    //다음 페이지의 존재 여부
+    private boolean next;
+
+    private List<E> dtoList;
+
+}
